7d4e454d84bdc25d374fb7b2e61f3674e995c35b
João Victor <j2003.victir@gmail.com>
2021-04-19 18:36:16 -0300

    Add global css reset and add new font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,53 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-	* {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
+	  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    list-style: none;
+    box-sizing: border-box;
+  }
+
+	html {
+    height: 100%;
+  }
+
+	:root {
+		--white: #fff;
+		--text:  #3A476C;
+		--text-secondary: #38466B;
+		--text-light: #78889E;
+		--text-hover: #E9316E;
+		--input-background: #F1F7FF;
+		--button: #E71D60;
+		--button-text: #FDEFF4;
+		--button-secondary: #FDE8EF;
+
 	}
 
 	body {
-		background: ${props => props.theme.colors.background};
-		color:${props => props.theme.colors.text};
-		font: 400 16px Roboto, sans-serif;
+		height: auto;
+		background: var(--white);
+		color: var(--text);
+		font: 400 16px Poppins, sans-serif;
+		-webkit-font-smoothing: antialiased !important;
+    -moz-osx-font-smoothing: grayscale;
 	}
+
+	#__next {
+		height: 100%;
+	}
+
+	body, input, button {
+    font: 1rem/1.48 'Poppins', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+  }
+
+	h1, h2, h3, strong {
+    color: var(--text-secondary);
+		font-weight: 700;
+
+  }
 `;
