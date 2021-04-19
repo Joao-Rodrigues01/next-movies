218a7eef9f5f46ed3b3f3bc7b91cc55ba2669928
João Victor <j2003.victir@gmail.com>
2021-04-19 01:30:00 -0300

    inital commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withImages = require('next-images')
+
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+}
+
+module.exports = 	withImages({
+	esModule: true,
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<Head>
+				<title>Home</title>
+
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<h1>Hello world</h1>
+			</main>
+		</div>
+	);
+};
+
+export default Home;
