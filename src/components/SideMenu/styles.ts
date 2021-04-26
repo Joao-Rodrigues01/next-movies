819e39c1ded7a30b8cbebb0bc3348049de3c08f6
João Victor <j2003.victir@gmail.com>
2021-04-26 00:07:39 -0300

    refactor: fixing some styles bugs in slidemenu

--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -5,22 +5,23 @@ export const Container = styled.aside`
 	width: 234px;
 	border-right: 2px solid #e4ecff;
 	padding-left: 40px;
-	padding-top: 30px;
+	padding-top: 10px;
 
 	h3 {
 		margin-bottom: 32px;
-		font-size: 18px;
+		font-size: 1rem;
 	}
 `;
 
 export const Menu = styled.nav`
 	display: flex;
 	flex-direction: column;
+	margin-bottom: 2rem;
 
 	h1 {
 		color: var(--text-light);
-		font-size: 1rem;
-		letter-spacing: 1px;
+		font-size: 0.9rem;
+		letter-spacing: 2px;
 	}
 
 	a {
@@ -28,18 +29,23 @@ export const Menu = styled.nav`
 		align-items: center;
 		justify-content: flex-start;
 
-		gap: 0.8rem;
+		gap: 1rem;
 
-		font-size: 0.9rem;
+		font-size: 0.85rem;
 		text-decoration: none;
 		color: var(--text);
 		font-weight: 700;
+		transition: filter 0.3s;
 
-		&:nth-child(2) {
-			color: #ef6b96;
-			border-right: 6px solid #ef6b96;
+		&.active {
+			color: #e9316e;
+			border-right: 4px solid #e9316e;
+		}
+
+		&:hover {
+			filter: brightness(0.65);
 		}
 	}
 
-	gap: 1.5rem;
+	gap: 1rem;
 `;
