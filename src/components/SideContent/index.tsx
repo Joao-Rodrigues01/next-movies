274fdd662ba83dc73b717eb035371d2b90acadb8
João Victor <j2003.victir@gmail.com>
2021-04-26 00:59:54 -0300

    Add PupolarMovies Section

--- src/components/SideContent/index.tsx
+++ src/components/SideContent/index.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
 import React from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 
-import { Container, SearchBarContainer } from './styles';
+import {
+	Container,
+	SearchBarContainer,
+	PopularMoviesContainer,
+	PopularMoviesCard,
+	PopularMoviesCardMovieInfo,
+} from './styles';
 
 const SideContent = () => {
 	return (
@@ -11,6 +18,44 @@ const SideContent = () => {
 				<FiSearch size={24} color="#3A476C" />
 				<input type="text" placeholder="Search" />
 			</SearchBarContainer>
+
+			<PopularMoviesContainer>
+				<h2>Popular Movies</h2>
+
+				<PopularMoviesCard>
+					<Image
+						src="/mortal.jpg"
+						alt="imagem dos filmes populares"
+						width={2000}
+						height={3000}
+						objectFit="cover"
+					/>
+					<PopularMoviesCardMovieInfo>
+						<p>Mortal Kombat</p>
+						<span>Action, Fantasy</span>
+					</PopularMoviesCardMovieInfo>
+
+					<span>PG - 13</span>
+				</PopularMoviesCard>
+
+				<PopularMoviesCard>
+					<Image
+						src="/kong.jpg"
+						alt="imagem dos filmes populares"
+						width={2000}
+						height={3000}
+						objectFit="cover"
+					/>
+					<PopularMoviesCardMovieInfo>
+						<p>Godzilla vs. Kong</p>
+						<span>Action, Fantasy</span>
+					</PopularMoviesCardMovieInfo>
+
+					<span>NC - 17</span>
+				</PopularMoviesCard>
+
+				<button type="button">See More</button>
+			</PopularMoviesContainer>
 		</Container>
 	);
 };
