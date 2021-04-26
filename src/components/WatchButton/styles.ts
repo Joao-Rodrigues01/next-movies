5e826361da47da3301db253ee3b26dc9fbd2a369
João Victor <j2003.victir@gmail.com>
2021-04-26 00:06:11 -0300

    feat: create WatchButton component

---
+++ src/components/WatchButton/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+	font-size: 0.9rem;
+	width: 9.75rem;
+	height: 2.7rem;
+	color: var(--white);
+	background: var(--button);
+`;
+
+export default Container;
