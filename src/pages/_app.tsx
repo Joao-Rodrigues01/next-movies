5f8bb423130931490e0f8eb9c7816f009b8340c1
João Victor <j2003.victir@gmail.com>
2021-04-28 18:05:00 -0300

    feat: add SideMenu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { AppProps } from 'next/app';
 
 import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
+import SideMenu from '../components/SideMenu';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
-			<Component {...pageProps} />
+			<Head>
+				<title>Next Movies</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main style={{ display: 'flex' }}>
+				<SideMenu />
+				<Component {...pageProps} />
+			</main>
 		</ThemeProvider>
 	);
 };
