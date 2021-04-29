065846eeaaf5b4ca1d0b9bed654edaa2181ba3f3
João Victor <j2003.victir@gmail.com>
2021-04-29 19:03:30 -0300

    add loading on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import NextProgress from 'nextjs-progressbar';
 
 import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
@@ -11,6 +12,12 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
+			<NextProgress
+				color="#E9316E"
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+			/>
 			<Head>
 				<title>Next Movies</title>
 				<link rel="icon" href="/favicon.ico" />
